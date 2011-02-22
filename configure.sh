522291ec76b8d2cf8ff1ead93a474d1d1595f08b
muffo87@gmail.com <muffo87@gmail.com@4693eacc-8f2f-b396-af3b-5490da26a7a6>
2011-02-22 17:31:11 +0000

    OpenCV + cv-test Sample images

--- configure.sh
+++ configure.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 
+echo "Config Overo ..."
+
 sed "s:OVEROTOP_MARK:$OVEROTOP:g" config/CMakeLists.txt > src/CMakeLists.txt
+
+echo "Clean previous build files"
+rm -rf bin
+rm -rf lib
+rm -rf build
 
 mkdir bin
 mkdir lib
@@ -9,7 +16,16 @@ mkdir build
 cd build
 cmake -G "Eclipse CDT4 - Unix Makefiles"  ../src/
 
+
+
+echo "Config Overo ..."
+
 cd ../pc-client
+
+echo "Clean previous build files"
+rm -rf bin
+rm -rf lib
+rm -rf build
 
 mkdir bin
 mkdir lib
---
+++ src/cv-test/cv-test.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <opencv/cv.h>
+#include <opencv/cxcore.h>
+#include <opencv/cvaux.h>
+#include <opencv/highgui.h>
+
+int main(int argc, char *argv[]) {
+	IplImage* img = 0;
+	int height, width, step, channels;
+	uchar *data;
+	int i, j, k;
+
+	if (argc < 2) {
+		printf("Usage: main <image-file-name>\n\7");
+		exit(0);
+	}
+
+	// load an image
+	img = cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR);
+	if (!img) {
+		printf("Could not load image file: %s\n", argv[1]);
+		exit(0);
+	}
+
+	// get the image data
+	height = img->height;
+	width = img->width;
+	step = img->widthStep;
+	channels = img->nChannels;
+	data = (uchar *) img->imageData;
+	printf("Processing a %dx%d image with %d channels\n", height, width,
+			channels);
+
+	// invert the image
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			for (k = 0; k < channels; k++)
+				data[i * step + j * channels + k] = 255 - data[i * step + j
+						* channels + k];
+
+	char newName[100];
+	sprintf(newName, "inv-%s", argv[1]);
+
+	cvSaveImage(newName, img, 0); // check if it works
+
+	// wait for a key
+	cvWaitKey(0);
+
+	// release the image
+	cvReleaseImage(&img);
+	return 0;
+}
